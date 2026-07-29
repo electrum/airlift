1a2944f660b3ed09f34ff29fab8e2bdcfab3f1d1
David Phillips <david@acz.org>
2026-07-29 14:12:40 -0700

    Make client info optional

--- mcp/src/main/java/io/airlift/mcp/operations/RequestMetadata.java
+++ mcp/src/main/java/io/airlift/mcp/operations/RequestMetadata.java
@@ -33,7 +33,7 @@ import static java.util.Objects.requireNonNull;
 
 public record RequestMetadata(
         Protocol protocol,
-        Implementation implementation,
+        Optional<Implementation> clientInfo,
         ClientCapabilities clientCapabilities,
         Optional<LoggingLevel> loggingLevel,
         Optional<Object> progressToken,
@@ -46,7 +46,7 @@ public record RequestMetadata(
     public RequestMetadata
     {
         requireNonNull(protocol, "protocol is null");
-        requireNonNull(implementation, "implementation is null");
+        requireNonNull(clientInfo, "clientInfo is null");
         requireNonNull(clientCapabilities, "clientCapabilities is null");
         requireNonNull(loggingLevel, "loggingLevel is null");
         requireNonNull(progressToken, "progressToken is null");
@@ -75,12 +75,12 @@ public record RequestMetadata(
         }
         Optional<String> mcpNameHeader = optional(request, HEADER_MCP_NAME);
 
-        Implementation implementation = required(jsonMapper, metadata, METADATA_CLIENT_INFO, Implementation.class);
+        Optional<Implementation> clientInfo = optional(jsonMapper, metadata, METADATA_CLIENT_INFO, Implementation.class);
         ClientCapabilities clientCapabilities = required(jsonMapper, metadata, METADATA_CLIENT_CAPABILITIES, ClientCapabilities.class);
         Optional<LoggingLevel> loggingLevel = optional(jsonMapper, metadata, METADATA_CLIENT_LOG_LEVEL, LoggingLevel.class);
         Optional<Object> progressToken = optional(jsonMapper, metadata, METADATA_PROGRESS_TOKEN, Object.class);
 
-        return new RequestMetadata(protocol, implementation, clientCapabilities, loggingLevel, progressToken, mcpNameHeader);
+        return new RequestMetadata(protocol, clientInfo, clientCapabilities, loggingLevel, progressToken, mcpNameHeader);
     }
 
     private static McpException unsupportedProtocol(String protocolVersionHeader)
